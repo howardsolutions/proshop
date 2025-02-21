a980c011873f74e9db8c33826c175387be1438b2
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-21 10:48:01 -0700

    feat: signin credentials form

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,9 @@
-export const APP_NAME =  process.env.NEXT_PUBLIC_APP_NAME || "ProShop";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ProShop";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Modern Ecommerce Fullstack App"
-export const SERVER_URL =  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
-export const LATEST_PRODUCTS_LIMIT =  Number(process.env.LATEST_PRODUCT_LIMIT || 4)
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCT_LIMIT || 4)
+
+export const signInDefaultValues = {
+    email: '',
+    password: '',
+};
